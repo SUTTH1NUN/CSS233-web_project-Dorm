c675a7bb0f12ac1bde3f485f21a2ae95c0ff7e2d
kanil-outis <thanakornkarndee@gmail.com>
2025-12-10 23:44:24 +0700

    update responsive 10/12

--- frontend/js/admin/script-announcement.js
+++ frontend/js/admin/script-announcement.js
@@ -30,4 +30,26 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+     const menuToggle = document.getElementById('menu-toggle');
+    const sidebar = document.getElementById('sidebar');
+    const overlay = document.getElementById('sidebar-overlay');
+
+    // 2. ตรวจสอบว่ามี Element ครบไหม
+    if (menuToggle && sidebar) {
+        
+        // เมื่อกดปุ่ม 3 ขีด
+        menuToggle.addEventListener('click', () => {
+            sidebar.classList.add('active'); // เติม class active เพื่อเลื่อนเมนูออกมา
+            if(overlay) overlay.classList.add('active'); // โชว์พื้นหลังดำ
+        });
+
+        // เมื่อกดพื้นหลังสีดำ (Overlay)
+        if(overlay) {
+            overlay.addEventListener('click', () => {
+                sidebar.classList.remove('active'); // ซ่อนเมนู
+                overlay.classList.remove('active'); // ซ่อนพื้นหลัง
+            });
+        }
+    }
+
 });
